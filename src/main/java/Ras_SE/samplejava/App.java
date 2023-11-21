e5168f091b7ce21190ab7eb91afe919cd9dc2e36
rasagna <chingaramrasagna@gmail.com>
2023-11-21 11:57:16 +0530

    mavenjava push

---
+++ src/main/java/Ras_SE/samplejava/App.java
@@ -0,0 +1,13 @@
+package Ras_SE.samplejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Ras_SE/samplejava/AppTest.java
@@ -0,0 +1,20 @@
+package Ras_SE.samplejava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
